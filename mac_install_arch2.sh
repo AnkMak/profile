db6700b4e7eab471fb669475908bfc9d970a8e72
AnkMak <ank.zgaf@gmail.com>
2017-10-15 23:22:09 +0800

    Create mac_install_arch2.sh

---
+++ mac_install_arch2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#-----check partition-----#
+#df -Th
+
+#-----Network Configuration-----#
+pacman -S --noconfirm iw wpa_supplicant dialog
+
+#-----Root password-----#
+passwd
+
+#-----Should be late-----#
+#vim /etc/locale.gen
+#locale-gen
+#echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+#echo "KEYMAP=us" >> /etc/vconsole.conf
+#echo "FONT=Lat2-Terminus16" >> /etc/vconsole.conf
+
+#echo MyArch > /etc/hostname
+
+#pacman -S --noconfirm zd1211-firmware
+
+#-----Boot loader-----#
+mkdir /boot/efi
+pacman -S --noconfirm grub
+pacman -S --noconfirm efibootmgr
+grub-install --target=x86_64efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck --debug
+cp /boot/efi/EFI/arch_grub/grubx64.efi /boot/backup.efi
+mkdir /boot/efi/EFI/BOOT
+mount LABEL=EFI /boot/efi
+cp /boot/backup.efi /boot/efi/EFI/BOOT/BOOTX64.efi
+#pacman -S --noconfirm os-prober
+grub-mkconfig -o /boot/grub/grub.cfg
+
+exit
+#umount -R /mnt
